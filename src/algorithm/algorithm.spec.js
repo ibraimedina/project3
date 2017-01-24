d8424086975387cc571fb155fd316162b5d62c46
Ibraim Medina <ibraimedina@gmail.com>
2017-01-24 21:57:41 -0200

    Add ESLint as dev dependency

--- src/algorithm/algorithm.spec.js
+++ src/algorithm/algorithm.spec.js
@@ -1,20 +1,22 @@
-var Algorithm = require(`./algorithm.js`);
+var Algorithm = require("./algorithm.js");
 
-describe(`Given a currency`, function() {	
-	
-	describe(`when has a "5" bill and "4.05" charge`, function() {
-		var bill = 5, charge = 4.05;
-		
-		it(`should change for 0.5+0.25+0.1+0.1`, function() {
+describe("Given a currency", function() {
+
+	describe("when has a '5' bill and '4.05' charge", function() {
+		var bill = 5,
+			charge = 4.05;
+
+		it("should change for 0.5+0.25+0.1+0.1", function() {
 			var change = [0.5, 0.25, 0.1, 0.1];
 			expect(Algorithm.bestChange(bill, charge)).toEqual(change);
 		});
 	});
 
-	describe(`when has a "5" bill and "4.13" charge`, function() {
-		var bill = 5, charge = 4.13;
-		
-		it(`should change for 0.5+0.25+0.1+0.05`, function() {
+	describe("when has a '5' bill and '4.13' charge", function() {
+		var bill = 5,
+			charge = 4.13;
+
+		it("should change for 0.5+0.25+0.1+0.05", function() {
 			var change = [0.5, 0.25, 0.1, 0.05];
 			expect(Algorithm.bestChange(bill, charge)).toEqual(change);
 		});
--- src/models/coin.spec.js
+++ src/models/coin.spec.js
@@ -1,27 +1,27 @@
-var Coin = require(`./coin.js`);
+var Coin = require("./coin.js");
 
-describe(`Given BRL currency`, function () {
-	var currency = `brl`;
+describe("Given BRL currency", function() {
+	var currency = "brl";
 
-	it(`should have 1, 0.5, 0.25, 0.1, 0.05 as options`, function () {
+	it("should have 1, 0.5, 0.25, 0.1, 0.05 as options", function() {
 		var options = [1, 0.5, 0.25, 0.1, 0.05];
 		expect(Coin.getCurrencyCoins(currency)).toEqual(options);
 	});
 });
 
-describe(`Given EUR currency`, function () {
-	var currency = `eur`;
+describe("Given EUR currency", function() {
+	var currency = "eur";
 
-	it(`should have 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 as options`, function() {
+	it("should have 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 as options", function() {
 		var options = [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
 		expect(Coin.getCurrencyCoins(currency)).toEqual(options);
 	});
 });
 
-describe(`Given USD currency`, function() {
-	var currency = `usd`;
+describe("Given USD currency", function() {
+	var currency = "usd";
 
-	it(`should have 1, 0.5, 0.25, 0.1, 0.05, 0.01 as options`, function() {
+	it("should have 1, 0.5, 0.25, 0.1, 0.05, 0.01 as options", function() {
 		var options = [1, 0.5, 0.25, 0.1, 0.05, 0.01];
 		expect(Coin.getCurrencyCoins(currency)).toEqual(options);
 	});
